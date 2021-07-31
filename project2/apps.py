1aeec709950cefcc5fadde3a333a1183211145b7
yoavhenig <33389702+yoavhenig@users.noreply.github.com>
2021-08-01 00:47:44 +0300

    Add files via upload

--- project2/apps.py
+++ project2/apps.py
@@ -1,22 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from keras.preprocessing.image import load_img
-from model import process_image, predict_class
+from predict import process_image, predict_class
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-app = Flask(__name__)
+item_names = {0: 'T-shirt/top', 1: 'Trouser', 2: 'Pullover',
+              3: 'Dress', 4: 'Coat', 5: 'Sandal', 6: 'Shirt',
+              7: 'Sneaker', 8: 'Bag', 9: 'Ankle boot'}
+
+app=Flask(__name__,template_folder='Template')
 photos = UploadSet('photos', IMAGES)
 
 app.config['UPLOADED_PHOTOS_DEST'] = './static/img'
-configure_uploads(apps, photos)
+configure_uploads(app, photos)
 
 @app.route('/upload', methods=['GET','POST'])
 def upload():
     if request.method == 'POST' and 'photo' in request.files:
         filename = photos.save(request.files['photo'])
-        image = load_img('./static/img' + filename, grayscale=True, target_size=(28,28))
+        image = load_img('./static/img/'+filename, grayscale=True, target_size=(28,28))
         image = process_image(image)
         pre_class, pre_prob = predict_class(image)
-        answer = "For {} : <br>classified as class: {} with propability of: {.6f}% ".format(filename, pre_class, pre_prob)
+        pre_prob = "{:.6f}".format(pre_prob)
+        answer = "For {} : <br>classified as class: {} with propability of: {}% ".format(filename, item_names[pre_class], pre_prob)
         return answer
 
     return render_template('upload.html')
---
+++ project2/predict.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+from keras.models import load_model
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg19 import preprocess_input
+from keras.applications.vgg19 import decode_predictions
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+model = tf.keras.models.load_model('./model1.h5', compile=False)
+
+def process_image(image):
+    image = img_to_array(image)
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    image = image.astype('float32')
+    image = image/255.0
+    return image
+
+
+def predict_class(image):
+    result_prob = model.predict(image)
+    result_class = model.predict_classes(image)
+
+    prediction = result_class[0]
+    percentage = (result_prob[0][prediction])
+
+    return prediction, percentage
+
+#image = load_img('sample_image.png',grayscale=True, target_size=(28,28))
+#image = process_image(image)
+#pre_class, pre_prob = predict_class(image)
+#print('Classified as class: {pre_class} with propability of: {a:.6f}% ')
